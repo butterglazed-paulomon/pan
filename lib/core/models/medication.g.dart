07b8c65ab6af003e6f451d958e9fefb77bf4441c
butterglazed-paulomon <butterglazed@gmail.com>
2025-07-18 23:36:20 +0800

    after build

---
+++ lib/core/models/medication.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'medication.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MedicationAdapter extends TypeAdapter<Medication> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Medication read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Medication(
+      name: fields[0] as String,
+      dose: fields[1] as String,
+      dosage: fields[2] as String,
+      frequency: fields[3] as String,
+      reminders: (fields[4] as List).cast<InvalidType>(),
+      notes: fields[5] as String?,
+      createdAt: fields[6] as DateTime?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Medication obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.dose)
+      ..writeByte(2)
+      ..write(obj.dosage)
+      ..writeByte(3)
+      ..write(obj.frequency)
+      ..writeByte(4)
+      ..write(obj.reminders)
+      ..writeByte(5)
+      ..write(obj.notes)
+      ..writeByte(6)
+      ..write(obj.createdAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MedicationAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
